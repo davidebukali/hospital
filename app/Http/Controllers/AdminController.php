149e8a23a741cb0248837c1e66e829aff423c3e2
David <debukali@gmail.com>
2026-02-05 14:19:37 +0300

    feat: Implement doctor creation functionality including form submission, image upload, and database persistence.

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,11 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Doctors;
 
 class AdminController extends Controller
 {
     public function AddDoctors()
     {
         return view('admin.add_doctors');
+    }
+
+    public function PostDoctors(Request $request)
+    {
+        $doctor = new Doctors();
+        $doctor->doctor_name = $request->doctor_name;
+        $doctor->doctor_phone = $request->doctor_phone;
+        $doctor->doctor_speciality = $request->doctor_speciality;
+        $doctor->room_number = $request->room_number;
+        $image = $request->file('doctor_image');
+        
+        if($image){
+            $imageName = time().'.'.$image->getClientOriginalExtension();
+            $doctor->doctor_image = $imageName;
+        }
+        $doctor->save();
+
+        if($image && $doctor->save()) {
+            $request->doctor_image->move(public_path('doctor_images'), $imageName);
+        }
+        return redirect()->back()->with('success', 'Doctor added successfully');
     }
 }
